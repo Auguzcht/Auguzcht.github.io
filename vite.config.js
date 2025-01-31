47afa5c7318a5d7c6bfe037f37fa6c1c88b801cc
Alfred <alfredndado@gmail.com>
2025-02-01 05:44:48 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/auguzcht.github.io/', // Change this to your GitHub repository name
+  base: './', 
   plugins: [react()],
 })
